f66e84c87b190c4b0e59d1c875eeb30178d6e00c
Vinícius Hack <viniciusvwh1441@gmail.com>
2022-08-19 22:13:54 -0300

    refact: useContextSelector

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,8 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import * as Dialog from '@radix-ui/react-dialog'
 import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
-import { useContext } from 'react'
 import { Controller, useForm } from 'react-hook-form'
+import { useContextSelector } from 'use-context-selector'
 import * as z from 'zod'
 import { TransactionsContext } from '../../contexts/TransactionsContext'
 import {
@@ -33,7 +33,12 @@ export function NewTransactionsModal() {
     resolver: zodResolver(newTransactionFormSchema),
   })
 
-  const { createTransaction } = useContext(TransactionsContext)
+  const createTransaction = useContextSelector(
+    TransactionsContext,
+    (context) => {
+      return context.createTransaction
+    },
+  )
 
   const handleCreateNewTransaction = async (data: NewTransactionFormInputs) => {
     const { category, description, price, type } = data
